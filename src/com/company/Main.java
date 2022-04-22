321ae94d0592c0e92ed8d9ddbb3e4edb3f4ffd92
Gaby <gaby.nyn@gmail.com>
2022-04-22 01:56:35 -0400

    Calcul moyenne WMC par version.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,17 +16,28 @@ public class Main {
         link = scanner.next();
         Path directory = Paths.get("LocalRep");
 
+        // Vérifie si LocalRep est vide, si non: delete
+        if (directory.toFile().listFiles() != null) {
+            Files.walk(directory).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+        }
+
+        // Si LocalRep n'existe pas, crée LocalRep
+        if (!directory.toFile().exists()){
+            directory.toFile().mkdirs();
+        }
+
         ArrayList<String> idVersionList = new ArrayList<>();
         ArrayList<Integer> nbClassList = new ArrayList<>();
+        ArrayList<Float> avgWmcList = new ArrayList<>();
         Metric metric = new Metric(directory, link);
-        metric.getData(idVersionList, nbClassList);
+        metric.getData(idVersionList, nbClassList, avgWmcList);
 
         // Crée lignes de données pour fichier csv
         List<String[]> ligneDonnees = new ArrayList<>();
-        ligneDonnees.add(new String[] {"id_version", "NC"});
+        ligneDonnees.add(new String[] {"id_version", "NC", "mWMC"});
         int counter = 0;
         while (counter < idVersionList.size()) {
-            ligneDonnees.add(new String[] {idVersionList.get(counter), nbClassList.get(counter).toString()});
+            ligneDonnees.add(new String[] {idVersionList.get(counter), nbClassList.get(counter).toString(), String.format("%2.04f", avgWmcList.get(counter))});
             counter++;
         }
 
@@ -36,7 +47,7 @@ public class Main {
         File fichierCSV = new File(resultDirectory, "results.csv");
         boolean verif = csv.creationFichierCSV(fichierCSV, ligneDonnees);
 
-        if (verif == true) {
+        if (verif) {
             System.out.println("Success.");
         } else {
             System.out.println("Fail.");
--- src/com/company/Metric.java
+++ src/com/company/Metric.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,11 +16,20 @@ public class Metric {
         this.link = link;
     }
 
-    public void getData(ArrayList<String> idVersionList, ArrayList<Integer> nbClassList) throws IOException, InterruptedException {
+    public void getData(ArrayList<String> idVersionList, ArrayList<Integer> nbClassList, ArrayList<Float> avgWmcList) throws IOException, InterruptedException {
+        // Cloner à local repository
         gitClone();
+
+        // Avoir id_version
         int nbCommits = gitCount();
         getIdVersions(idVersionList, nbCommits);
+
+        // Avoir nombre de classes
         getNbClasse(nbClassList, idVersionList);
+
+        // Avoir moyenne wmc par version
+        getWmcAverage(avgWmcList, idVersionList);
+
     }
 
     public void gitClone() throws IOException, InterruptedException {
@@ -31,11 +37,13 @@ public class Metric {
         ProcessBuilder gitClone = new ProcessBuilder().command("git", "clone", link).directory(directory.toFile());
         Process process = gitClone.start();
         try{
+            System.out.println("Processing data, this might take a while.");
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new InterruptedException("Wait interrupted.");
         }
+        process.destroy();
     }
 
     public int gitCount() throws IOException {
@@ -46,12 +54,14 @@ public class Metric {
         ProcessBuilder gitCount = new ProcessBuilder().command("git", "rev-list","--count", "master").directory(cloneDirectory.toFile());
         Process processGitCount = gitCount.start();
         BufferedReader stdInput = new BufferedReader(new InputStreamReader(processGitCount.getInputStream()));
-        int nbCommit = 0;
+        int nbCommit;
         try {
             nbCommit = Integer.parseInt(stdInput.readLine());
         } catch (IOException e) {
             throw new IOException("Couldn't get count.");
         }
+        processGitCount.destroy();
+        stdInput.close();
         return nbCommit;
     }
 
@@ -74,10 +84,12 @@ public class Metric {
         Collections.shuffle(idVersionListTemp);
         int bound = (int)(nbCommit*(5.0f/100.0f));
         int boundCount = 0;
-        while (boundCount < bound) {
+        while (boundCount < bound && boundCount < idVersionListTemp.size()) {
             idVersionList.add(idVersionListTemp.get(boundCount));
             boundCount++;
         }
+        stdInput.close();
+        processRevList.destroy();
     }
 
     public void getNbClasse(ArrayList<Integer> nbClassList, ArrayList<String> idVersionList) throws IOException {
@@ -87,21 +99,131 @@ public class Metric {
         for (String s : idVersionList) {
             ProcessBuilder gitReset = new ProcessBuilder().command("git", "reset","--HARD", s).directory(cloneDirectory.toFile());
             Process processGitReset = gitReset.start();
-            nbClassList.add(getFichiers(cloneDirectory.toFile()));
+            nbClassList.add(getFichiersCount(cloneDirectory.toFile()));
+            processGitReset.destroy();
         }
+    }
+
+    public void getWmcAverage(ArrayList<Float> avgWmcList, ArrayList<String> idVersionList) throws IOException {
+        // Compte nombre de classe par commit
+        File[] files = directory.toFile().listFiles();
+        Path cloneDirectory = files[0].toPath();
+        ArrayList<File> filesList = new ArrayList<>();
+        for (String s : idVersionList) {
+            ProcessBuilder gitReset = new ProcessBuilder().command("git", "reset","--HARD", s).directory(cloneDirectory.toFile());
+            Process processGitReset = gitReset.start();
+
+            getFichiers(dirToFile, filesList);
+
+            ArrayList<Integer> complexityList = new ArrayList<>();
+            for (File f : filesList) {
+                complexityList.add(getWmc(f.getAbsolutePath()));
+            }
+
+            float average = 0;
+            for (float i : complexityList) {
+                average += i;
+            }
+            avgWmcList.add(average / complexityList.size());
+            processGitReset.destroy();
+        }
+    }
+
+    /**
+     * getWmc calcule la somme pondérée des complexité cyclomatiques de McCabe de toutes les méthodes et classes.
+     *
+     * @param chemin est la valeur/chemin du fichier a valider
+     * @return retourne le nombre de ligne dans le fichier
+     */
+    public int getWmc(String chemin) {
+        //Déclaration des variables
+        String ligne = "";
+        int noeud = 1;
+        int arete = 0;
+        File fichierCible = new File(chemin);
+
+        //Vérification si les lignes contiennent les items pour le calcul de WMC et incrémenter les valeurs arete et noeud
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fichierCible));
+            while ((ligne = br.readLine()) != null) {
+                if (ligne.contains("print") || ligne.contains("return")) {
+                    arete++;
+                    noeud++;
+                } else if (ligne.contains("if") ||
+                        ligne.contains("else") ||
+                        ligne.contains("while") ||
+                        ligne.contains("do") ||
+                        ligne.contains("for") ||
+                        ligne.contains("try") ||
+                        ligne.contains("catch")) {
+                    try {
+                        while (!ligne.contains("}") && !(ligne = br.readLine()).contains("}")) {
+                            arete++;
+                            noeud++;
+                        }
+                    } catch (NullPointerException e) {
+                        break;
+                    }
+                    arete++;
+                } else if (ligne.contains("switch")) {
+                    try {
+                        do {
+                            ligne = br.readLine();
+                            if (!ligne.contains("case")) {
+                                arete++;
+                                noeud++;
+                            }
+                        } while (!ligne.contains("default"));
+
+                        while (!ligne.contains("}") && !(ligne = br.readLine()).contains("}")) {
+                            arete++;
+                            noeud++;
+                        }
+                    } catch (NullPointerException e) {
+                        break;
+                    }
+                    arete++;
+                } else if (ligne.contains("}") &&
+                        (ligne.contains("if") ||
+                                ligne.contains("else") ||
+                                ligne.contains("while") ||
+                                ligne.contains("do") ||
+                                ligne.contains("for") ||
+                                ligne.contains("try") ||
+                                ligne.contains("catch"))) {
+                    try {
+                        while (!ligne.contains("}") && !(ligne = br.readLine()).contains("}")) {
+                            arete++;
+                            noeud++;
+                        }
+                    } catch (NullPointerException e) {
+                        break;
+                    }
+                    arete++;
+                }
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return 0;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+        return arete - noeud + 2;
     }
 
 
     /**
-     * Méthode pour aller chercher tous les fichiers dans le directory donnée.
+     * Méthode pour aller compter tous les fichiers dans le directory donnée.
      * @param dir directory cible
      */
-    public int getFichiers(File dir) {
+    public int getFichiersCount(File dir) {
         int count = 0;
         if (dir.isDirectory()) {
             File[] listeFichiers = dir.listFiles();
             for (File f : listeFichiers) {
-                count += getFichiers(f);
+                count += getFichiersCount(f);
             }
         } else {
             if(dir.getName().endsWith(".java")) {
@@ -109,5 +231,23 @@ public class Metric {
             }
         }
         return count;
+    }
+
+    /**
+     * Méthode pour aller chercher tous les fichiers dans le directory donnée.
+     * @param dir directory cible
+     * @param liste liste dans laquelle on met tous les fichiers
+     */
+    public void getFichiers(File dir, ArrayList<File> liste) {
+        if (dir.isDirectory()) {
+            File[] listeFichiers = dir.listFiles();
+            for (File f : listeFichiers) {
+                getFichiers(f, liste);
+            }
+        } else {
+            if(dir.getName().endsWith(".java")) {
+                liste.add(dir);
+            }
+        }
     }
 }
